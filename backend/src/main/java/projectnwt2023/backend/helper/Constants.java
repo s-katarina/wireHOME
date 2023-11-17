a44a4a2bfa72df1710fe56dee21a4c3082768e4f
miloss01 <mikicamiki.bat@gmail.com>
2023-11-17 15:09:22 +0100

    Change constants file

--- backend/src/main/java/projectnwt2023/backend/helper/Constants.java
+++ backend/src/main/java/projectnwt2023/backend/helper/Constants.java
@@ -2,7 +2,7 @@ package projectnwt2023.backend.helper;
 
 public class Constants {
 
-    public static String sendgridKey = "E8Zm[5v94xW9";
+    public static String sendgridKey = "";
     public static String fromEmail = "project.nwt2023@gmail.com";
     public static String superAdminPath = "./src/main/resources/superadmin.txt";
     public static String profileImageFolderPath = "./profileImages";
