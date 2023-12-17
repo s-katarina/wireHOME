4c1cb4b915c4ab45f240ffe926a1491971178c73
miloss01 <mikicamiki.bat@gmail.com>
2023-12-17 14:14:17 +0100

    Add saving current action and temp and starting from that data

--- backend/src/main/java/projectnwt2023/backend/devices/service/AirConditionerService.java
+++ backend/src/main/java/projectnwt2023/backend/devices/service/AirConditionerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import projectnwt2023.backend.devices.AirConditioner;
 import projectnwt2023.backend.devices.repository.DeviceRepository;
 import projectnwt2023.backend.devices.service.interfaces.IAirConditionerService;
 
@@ -25,6 +26,10 @@ public class AirConditionerService implements IAirConditionerService {
             String response = payload.split(";")[0];
             Integer deviceId = Integer.valueOf(payload.split(";")[1]);
             simpMessagingTemplate.convertAndSend("/air-conditioner/" + deviceId + "/response", response);
+
+            AirConditioner airConditioner = (AirConditioner) deviceRepository.findById(deviceId.longValue()).get();
+            airConditioner.setCurrentAction(response);
+            deviceRepository.save(airConditioner);
         }
 
         if (isStringMatchingPattern(topic, "airConditioner/\\d+/temp")) {
@@ -32,6 +37,10 @@ public class AirConditionerService implements IAirConditionerService {
             String temp = payload.split(";")[0];
             Integer deviceId = Integer.valueOf(payload.split(";")[1]);
             simpMessagingTemplate.convertAndSend("/air-conditioner/" + deviceId + "/temp", temp);
+
+            AirConditioner airConditioner = (AirConditioner) deviceRepository.findById(deviceId.longValue()).get();
+            airConditioner.setTemp(Integer.valueOf(temp));
+            deviceRepository.save(airConditioner);
         }
     }
 
