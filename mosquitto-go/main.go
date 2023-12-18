43013915800c453689da0932293795b0f83feafc
SelenaMilutin <selena.milutin@gmail.com>
2023-12-18 22:10:09 +0100

    Fix sheduler

--- mosquitto-go/main.go
+++ mosquitto-go/main.go
@@ -4,11 +4,11 @@ import (
 	// "fmt"
 	"sync"
 	battery "tim10/mqtt/battery"
-	"tim10/mqtt/gate"
+	// "tim10/mqtt/gate"
 	lamp "tim10/mqtt/lamp"
 	solarPanel "tim10/mqtt/solarPanel"
-	ambientSensor "tim10/mqtt/ambientSensor"
-	airConditioner "tim10/mqtt/airConditioner"
+	// ambientSensor "tim10/mqtt/ambientSensor"
+	// airConditioner "tim10/mqtt/airConditioner"
 )
 
 // ambientSensore "tim10/mqtt/ambientSensore"
@@ -18,13 +18,13 @@ func main() {
 	var wg sync.WaitGroup
 
 	// Add the number of goroutines you want to wait for
-	wg.Add(6)
-	go gate.RunGate()
+	wg.Add(3)
+	// go gate.RunGate()
 	go lamp.RunLamp()
 	go battery.RunBattery(3)
-	go solarPanel.RunSolarPanel(4);
-	go airConditioner.RunAirConditioner()
-	go ambientSensor.RunAmbientSensor();
+	go solarPanel.RunSolarPanel(1);
+	// go airConditioner.RunAirConditioner()
+	// go ambientSensor.RunAmbientSensor();
 	wg.Wait()
 
 	// fmt.Println("All scripts completed")
