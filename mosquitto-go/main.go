1d5dfb810f4b3c1ac702c60a637d66fa26e1b556
unknown <spremic.katarina@gmail.com>
2024-01-31 21:04:24 +0100

    Fix chart online/offline style

--- mosquitto-go/main.go
+++ mosquitto-go/main.go
@@ -8,9 +8,9 @@ import (
 	// "tim10/mqtt/gate"
 	// lamp "tim10/mqtt/lamp"
 	// solarPanel "tim10/mqtt/solarPanel"
-	// ambientSensor "tim10/mqtt/ambientSensor"
+	ambientSensor "tim10/mqtt/ambientSensor"
 	// airConditioner "tim10/mqtt/airConditioner"
-	charger "tim10/mqtt/charger"
+	// charger "tim10/mqtt/charger"
 	// airConditioner "tim10/mqtt/airConditioner"
 	// ambientSensor "tim10/mqtt/ambientSensor"
 	// battery "tim10/mqtt/battery"
@@ -34,13 +34,13 @@ func main() {
 	// go airConditioner.RunAirConditioner()
 	// go ambientSensor.RunAmbientSensor();
 	// go charger.RunCharger(6)
-	go charger.RunCharger(9)
+	// go charger.RunCharger(9)
 	// go gate.RunGate()
 	// go lamp.RunLamp()
 	// go battery.RunBattery(3)
 	// go solarPanel.RunSolarPanel(4);
 	// go airConditioner.RunAirConditioner()
-	// go ambientSensor.RunAmbientSensor();
+	go ambientSensor.RunAmbientSensor();
 	// go sprinkler.RunSprinkler();
 	// go gate.RunGate()
 	// go lamp.RunLamp()
